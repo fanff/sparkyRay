23fb0a9d4dc8d952914fd7c235d054772d3a7354
fanf <fanf@fanf>
2020-10-20 12:29:45 +0200

    iup

--- testray.py
+++ testray.py
@@ -1,17 +1,19 @@
 
 
 import ray
+import numpy as np
+import unittest
+
+class SimpleTest(unittest.TestCase):
+
+    def test_1(self):
+        scene = []
+
+        rayO = np.array([0,0,0])
+        rayD = np.array([1,0,0])
 
 
-scene = []
+        L = np.array([5., 5., -10.])
 
 
-rayO = np.array([0,0,0])
-rayD = np.array([1,0,0])
-
-
-
-L = np.array([5., 5., -10.])
-
-
-ray.raycalc(scene,  )
+        ray.raycalc(scene,  )
