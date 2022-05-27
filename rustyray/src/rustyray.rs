e9ac1eaf82008e3dd778f877699a72eb5f0689b9
fanf <fanf@fanf>
2022-05-27 14:50:34 +0200

    init a rust version

--- rustyray/src/rustyray.rs
+++ rustyray/src/rustyray.rs
@@ -6,4 +6,14 @@ pub struct Vec3f{
 }
 
 
+pub fn add_vec(vec1:Vec3f,vec2: Vec3f)-> Vec3f{
+    // add two vector
+    return Vec3f{x : vec1.x+vec2.x,
+                y : vec1.y+vec2.y,
+                z : vec1.z+vec2.z
+    }
+}
 
+pub fn norm_vec_2(v:Vec3f) -> f32 {
+    return v.x.powf(2.0) + v.y.powf(2.0) + v.z.powf(2.0)
+}
--- rustyray/tests/rustray_test.rs
+++ rustyray/tests/rustray_test.rs
@@ -1,6 +1,8 @@
 
 // use rustyray::Vec3f;
+mod rustyray;
 
+use rustyray::norm_vec_2;
 
 #[cfg(test)]
 mod tests {
@@ -9,8 +11,12 @@ mod tests {
     #[test]
     fn test_1() {
         let v1:rustyray::Vec3f =
-            rustyray::Vec3f{x:2.0,y:1.0,z:2.0};
+            rustyray::Vec3f{x:1.0,y:0.0,z:0.0};
 
         dbg!((v1.x, v1.y,v1.z));
+
+
+         assert_eq!(1.0, norm_vec_2(v1));
+
     }
 }
